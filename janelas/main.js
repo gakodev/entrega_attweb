82c25f50230b80d5a375e50e4a1ef7b20b35116c
gakodev <138790246+gakodev@users.noreply.github.com>
2023-11-25 12:36:14 -0300

    att 25.11

---
+++ janelas/main.js
@@ -0,0 +1,30 @@
+const campo2 =  document.getElementById('telefoneInput');
+campo2.addEventListener('keyup', formatarTelefone);
+
+const input4 = document.getElementById('cpfInput');
+input4.addEventListener('keyup', formatarCPF);
+
+const input3 = document.getElementById('dataInput');
+input3.addEventListener('keyup', formatarData);
+
+function formatarTelefone(e){
+    var v=e.target.value.replace(/\D/g,"");
+    v=v.replace(/^(\d\d)(\d)/g,"($1)$2"); 
+    v=v.replace(/(\d{5})(\d)/,"$1-$2");    
+    e.target.value = v;
+}
+
+function formatarData(e){
+    var v=e.target.value.replace(/\D/g,"");
+    v=v.replace(/(\d{2})(\d)/,"$1/$2") 
+    v=v.replace(/(\d{2})(\d)/,"$1/$2") 
+    e.target.value = v;
+}
+
+function formatarCPF(e){
+    var v=e.target.value.replace(/\D/g,"");
+    v=v.replace(/(\d{3})(\d)/,"$1.$2");
+    v=v.replace(/(\d{3})(\d)/,"$1.$2");
+    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+    e.target.value = v;
+}
